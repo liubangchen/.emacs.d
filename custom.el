767828bbc2823706be83ce6e75dbfc039e1c1776
liubangchen <liubangchen@tencent.com>
2021-05-14 03:54:53 +0000

    chang font size

--- custom.el
+++ custom.el
@@ -35,7 +35,7 @@
            when (font-installed-p font)
            return (set-face-attribute 'default nil
                                       :font font
-                                      :height (cond (sys/mac-x-p 180)
+                                      :height (cond (sys/mac-x-p 160)
                                                     (sys/win32p 110)
                                                     (t 100))))
 
