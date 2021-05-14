2562ab6c0c8494a200c252c772eebe40e217333a
liubangchen <liubangchen@tencent.com>
2021-05-14 06:32:13 +0000

    update taskjuggle tjp path

--- lisp/init-taskjuggle.el
+++ lisp/init-taskjuggle.el
@@ -1,6 +1,6 @@
 (require 'taskjuggler-mode)
 (require 'ox-taskjuggler)
-(setq org-taskjuggler-default-reports '("include \"~/.emacs.d/lisp/reports.tji\""))
+(setq org-taskjuggler-default-reports '("include \"/Users/chenlong/.emacs.d/lisp/reports.tji\""))
 
 (add-hook 'org-mode-hook (lambda()
                            (require 'ox-taskjuggler)
