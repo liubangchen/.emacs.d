0c6bcc1be6a0c992a573f32b422943b533df7651
liubangchen <liubangchen@tencent.com>
2021-09-28 15:58:07 +0800

    nots

--- lisp/init-function.el
+++ lisp/init-function.el
@@ -79,4 +79,26 @@
 (add-hook 'sh-mode-hook         'hs-minor-mode)
 (add-hook 'python-mode-hook         'hs-minor-mode)
 
+(setq org-html-scripts "<![CDATA[/*><!--*/
+ function CodeHighlightOn(elem, id)
+ {
+   var target = document.getElementById(id);
+   if(null != target) {
+     elem.cacheClassElem = elem.className;
+     elem.cacheClassTarget = target.className;
+     target.className = \"code-highlighted\";
+     elem.className   = \"code-highlighted\";
+   }
+ }
+ function CodeHighlightOff(elem, id)
+ {
+   var target = document.getElementById(id);
+   if(elem.cacheClassElem)
+     elem.className = elem.cacheClassElem;
+   if(elem.cacheClassTarget)
+     target.className = elem.cacheClassTarget;
+ }
+/*]]>*///-->
+</script>")
+
 (provide 'init-function)
