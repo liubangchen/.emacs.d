680b0c534856dbb1a9bdbed29da338bd042ec189
liubangchen <liubangchen@tencent.com>
2021-12-28 13:30:24 +0800

    treemacs

--- custom.el
+++ custom.el
@@ -29,27 +29,28 @@
 ;; (setq desktop-base-lock-name (format ".emacs-%s.desktop.lock" emacs-major-version))
 
 ;; Fonts
-;; (when (display-graphic-p)
-;;   ;; Set default font
-;;   (cl-loop for font in '("Monaco" "Sarasa Mono SC" "SF Mono" "Hack" "Source Code Pro" "Fira Code"
-;;                          "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
-;;            when (font-installed-p font)
-;;            return (set-face-attribute 'default nil
-;;                                       :font font
-;;                                       :height (cond (sys/mac-x-p 130)
-;;                                                     (sys/win32p 110)
-;;                                                     (t 100))))
-
-;;   ;; Specify font for all unicode characters
-;;   (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
-;;            when (font-installed-p font)
-;;            return(set-fontset-font t 'unicode font nil 'prepend))
-
-;;   ;; Specify font for Chinese characters
-;;   (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-;;            when (font-installed-p font)
-;;            return (set-fontset-font t '(#x4e00 . #x9fff) font)))
-
+;;(when (display-graphic-p)
+;;  ;; Set default font
+;;  (cl-loop for font in '("Sarasa Mono SC" "SF Mono" "Hack" "Source Code Pro" "Fira Code"
+;;                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+;;           when (font-installed-p font)
+;;           return (set-face-attribute 'default nil
+;;                                      :font font
+;;                                      :height (cond (sys/mac-x-p 130)
+;;                                                    (sys/win32p 110)
+;;                                                    (t 100))))
+;;
+;;  ;; Specify font for all unicode characters
+;;  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+;;           when (font-installed-p font)
+;;           return(set-fontset-font t 'unicode font nil 'prepend))
+;;
+;;  ;; Specify font for Chinese characters
+;;  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+;;           when (font-installed-p font)
+;;           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+;;
+;;
 (defun set-font (english chinese english-size chinese-size)
   (set-face-attribute 'default nil :font
                       (format   "%s:pixelsize=%d"  english english-size))
@@ -57,9 +58,9 @@
     (set-fontset-font (frame-parameter nil 'font) charset
                       (font-spec :family chinese :size chinese-size))))
 
-;;(set-font   "Monaco" "Hiragino Sans GB" 14 16)
-(if window-system
-    (set-font   "Sarasa Mono SC" "Sarasa Mono SC" 15 15))
+(set-font   "Monaco" "Hiragino Sans GB" 14 16)
+;;(if window-system
+;;    (set-font   "Sarasa Mono SC" "Sarasa Mono SC" 15 15))
 ;;(set-font   "Sarasa Mono SC" "Sarasa Mono SC" 16 16)
 
 ;; Mail
