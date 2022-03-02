33792aab93a75c6fcc18b176e41ad27bbba1421a
liubangchen <liubangchen@tencent.com>
2022-03-02 20:56:02 +0800

    ccls

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -573,7 +573,7 @@
        :defines projectile-project-root-files-top-down-recurring
        :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda () (require 'ccls)))
        :config
-       (setq ccls-initialization-options '(:index (:comments 2 :onChange t :threads 6) :completion (:detailedLabel t)))
+       (setq ccls-initialization-options '(:index (:threads 6) :completion (:detailedLabel t)))
        (with-eval-after-load 'projectile
          (setq projectile-project-root-files-top-down-recurring
                (append '("compile_commands.json" ".ccls")
