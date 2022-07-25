cc1abaf41ce7897bc1b9cd3ad7b29adfb385f8b4
liubangchen <20183398@qq.com>
2022-07-25 09:44:24 +0800

    merge init-player.el

--- lisp/init-player.el
+++ lisp/init-player.el
@@ -78,12 +78,12 @@
     (setq simple-mpc-playlist-format "[[%artist% - ]%title%]|[%file%]")
 
     (defun simple-mpc-play ()
-      "Play the song."
+      "Start playing the song."
       (interactive)
       (simple-mpc-call-mpc nil "play"))
 
     (defun simple-mpc-stop ()
-      "Stop the song."
+      "Stop the playback."
       (interactive)
       (simple-mpc-call-mpc nil "stop"))
 
@@ -101,12 +101,12 @@
                             (info-strs (split-string info))
                             (state (nth 0 info-strs))
                             (time (nth 2 info-strs)))
-                  (propertize (format "%s%s [%s] "
-                                      (and (icon-displayable-p)
-                                           (pcase state
-                                             ("[playing]" " ")
-                                             ("[paused]" " ")
-                                             (_ "")))
+                  (propertize (format " %s%s [%s] "
+                                      (when (icon-displayable-p)
+                                        (pcase state
+                                          ("[playing]" " ")
+                                          ("[paused]" " ")
+                                          (_ "")))
                                       title time)
                               'face 'font-lock-comment-face)))))
       (force-mode-line-update))
@@ -114,7 +114,7 @@
     (defvar simple-mpc--timer nil)
     (defun simple-mpc-start-timer ()
       "Start simple-mpc timer to refresh current song."
-      (setq simple-mpc--timer (run-with-timer 0 1 #'simple-mpc-current)))
+      (setq simple-mpc--timer (run-with-timer 1 1 #'simple-mpc-current)))
     (defun simple-mpc-stop-timer ()
       "Stop simple-mpc timer."
       (when (timerp simple-mpc--timer)
