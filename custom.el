0959ea9f22d33a2405e0cd9e5c8954e564f27af2
liubangchen <20183398@qq.com>
2022-10-21 13:52:54 +0800

    更换定时器

--- custom.el
+++ custom.el
@@ -4,22 +4,23 @@
 ;;;       Put your own configurations in custom-post.el to override default configurations.
 ;;; Code:
 
-(setq centaur-logo nil)                        ; Logo file or nil (official logo)
+;;(setq centaur-logo nil)                        ; Logo file or nil (official logo)
 (setq centaur-full-name "liubangchen")           ; User full name
-(setq centaur-mail-address  "liubangchen@tencent.com")   ; Email address
-;; (setq centaur-proxy "127.0.0.1:1080")          ; Network proxy
+(setq centaur-mail-address "liubangchen@tencent.com")   ; Email address
+;; (setq centaur-proxy "127.0.0.1:7890")          ; HTTP/HTTPS proxy
+;; (setq centaur-socks-proxy "127.0.0.1:7890")    ; SOCKS proxy
 ;; (setq centaur-server nil)                      ; Enable `server-mode' or not: t or nil
-(setq centaur-icon t)                        ; Display icons or not: t or nil
-(setq centaur-package-archives 'melpa)         ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
-(setq centaur-theme 'default)      ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night doom-solarized-dark doom-monokai-classic
-(setq centaur-completion-style 'minibuffer)    ; Completion display style: minibuffer or childframe
-(setq centaur-org-directory "~/notes/org")
-;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
-(setq centaur-restore-frame-geometry t)      ; Restore the frame's geometry at startup: t or nil
+;; (setq centaur-icon nil)                        ; Display icons or not: t or nil
+(setq centaur-package-archives 'melpa)         ; Package repo: melpa, emacs-cn, bfsu, netease, sjtu, tencent, tuna or ustc
+(setq centaur-theme 'dark)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
+;; (setq centaur-completion-style 'minibuffer)    ; Completion display style: minibuffer or childframe
+;; (setq centaur-dashboard nil)                   ; Display dashboard at startup or not: t or nil
+;; (setq centaur-restore-frame-geometry nil)      ; Restore the frame's geometry at startup: t or nil
 (setq centaur-lsp 'lsp-mode)                      ; Set LSP client: lsp-mode, eglot or nil
-(setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode java-mode go-mode)) ; Ignore format on save for some languages
-(setq centaur-chinese-calendar t)              ; Use Chinese calendar or not: t or nil
-(setq package-check-signature nil)
+(setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode python-mode markdown-mode java-mode)) ; Ignore format on save for some languages
+(setq centaur-tree-sitter t)                   ; Enable `tree-sitter' or not: t or nil
+(setq centaur-chinese-calendar t)              ; Support Chinese calendar or not: t or nil
+(setq centaur-player t)                        ; Enable players or not: t or nil
 ;; (setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
 ;; (setq centaur-prettify-org-symbols-alist nil)  ; Alist of symbol prettifications for `org-mode'
 
@@ -29,40 +30,57 @@
 ;; (setq desktop-base-lock-name (format ".emacs-%s.desktop.lock" emacs-major-version))
 
 ;; Fonts
-;;(when (display-graphic-p)
-;;  ;; Set default font
-;;  (cl-loop for font in '("Sarasa Mono SC" "SF Mono" "Hack" "Source Code Pro" "Fira Code"
-;;                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
-;;           when (font-installed-p font)
-;;           return (set-face-attribute 'default nil
-;;                                      :font font
-;;                                      :height (cond (sys/mac-x-p 130)
-;;                                                    (sys/win32p 110)
-;;                                                    (t 100))))
-;;
-;;  ;; Specify font for all unicode characters
-;;  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
-;;           when (font-installed-p font)
-;;           return(set-fontset-font t 'unicode font nil 'prepend))
-;;
-;;  ;; Specify font for Chinese characters
-;;  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-;;           when (font-installed-p font)
-;;           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
-;;
-;;
-(defun set-font (english chinese english-size chinese-size)
-  (set-face-attribute 'default nil :font
-                      (format   "%s:pixelsize=%d"  english english-size))
-  (dolist (charset '(kana han symbol cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font) charset
-                      (font-spec :family chinese :size chinese-size))))
+(defun centaur-setup-fonts ()
+  "Setup fonts."
+  (when (display-graphic-p)
+    ;; Set default font
+    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
+                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
+                           "Monaco" "DejaVu Sans Mono" "Consolas")
+             when (font-installed-p font)
+             return (set-face-attribute 'default nil
+                                        :family font
+                                        :height (cond (sys/macp 160)
+                                                      (sys/win32p 110)
+                                                      (t 100))))
 
-(if (display-graphic-p)
-    (set-font   "Monaco" "Hiragino Sans GB" 14 16))
-;;(if window-system
-;;    (set-font   "Sarasa Mono SC" "Sarasa Mono SC" 15 15))
-;;(set-font   "Sarasa Mono SC" "Sarasa Mono SC" 16 16)
+    ;; Set mode-line font
+    ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
+    ;;          when (font-installed-p font)
+    ;;          return (progn
+    ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
+    ;;                   (when (facep 'mode-line-active)
+    ;;                     (set-face-attribute 'mode-line-active nil :family font :height 120))
+    ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
+
+    ;; Specify font for all unicode characters
+    (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
+             when (font-installed-p font)
+             return (if (< emacs-major-version 27)
+                        (set-fontset-font "fontset-default" 'unicode font nil 'prepend)
+                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
+
+    ;; Emoji
+    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji" "Segoe UI Emoji")
+             when (font-installed-p font)
+             return (cond
+                     ((< emacs-major-version 27)
+                      (set-fontset-font "fontset-default" 'unicode font nil 'prepend))
+                     ((< emacs-major-version 28)
+                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend))
+                     (t
+                      (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))))
+
+    ;; Specify font for Chinese characters
+    (cl-loop for font in '("WenQuanYi Micro Hei" "PingFang SC" "Microsoft Yahei" "STFangsong")
+             when (font-installed-p font)
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.3)))
+                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
+
+(centaur-setup-fonts)
+(add-hook 'window-setup-hook #'centaur-setup-fonts)
+(add-hook 'server-after-make-frame-hook #'centaur-setup-fonts)
 
 ;; Mail
 ;; (setq message-send-mail-function 'smtpmail-send-it
@@ -84,9 +102,25 @@
 
 ;; Enable proxy
 ;; (proxy-http-enable)
+;; (proxy-socks-enable)
 
 ;; Display on the specified monitor
 ;; (when (and (> (length (display-monitor-attributes-list)) 1)
 ;;            (> (display-pixel-width) 1920))
 ;;   (set-frame-parameter nil 'left 1920))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;;; custom.el ends here
