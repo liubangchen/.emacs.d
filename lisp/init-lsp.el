7e3699b38b4dcbddb87f499ad36e0572c49c0f00
liubangchen <20183398@qq.com>
2023-01-29 10:02:50 +0800

    trino

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -38,9 +38,9 @@
   ('eglot
    (use-package eglot
      :hook ((prog-mode . (lambda ()
-                           (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode)
+                           (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode 'snippet-mode)
                              (eglot-ensure))))
-            ((markdown-mode yaml-mode) . eglot-ensure))))
+            ((markdown-mode yaml-mode yaml-ts-mode) . eglot-ensure))))
   ('lsp-mode
    ;; Performace tuning
    ;; @see https://emacs-lsp.github.io/lsp-mode/page/performance/
@@ -55,9 +55,9 @@
      :autoload lsp-enable-which-key-integration
      :commands (lsp-format-buffer lsp-organize-imports)
      :hook ((prog-mode . (lambda ()
-                           (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode)
+                           (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode 'snippet-mode)
                              (lsp-deferred))))
-            ((markdown-mode yaml-mode) . lsp-deferred)
+            ((markdown-mode yaml-mode yaml-ts-mode) . lsp-deferred)
             (lsp-mode . (lambda ()
                           ;; Integrate `which-key'
                           (lsp-enable-which-key-integration)
@@ -105,9 +105,10 @@
 
        ;; Enable `lsp-mode' in sh/bash/zsh
        (defun my-lsp-bash-check-sh-shell (&rest _)
-         (and (eq major-mode 'sh-mode)
+         (and (memq major-mode '(sh-mode bash-ts-mode))
               (memq sh-shell '(sh bash zsh))))
        (advice-add #'lsp-bash-check-sh-shell :override #'my-lsp-bash-check-sh-shell)
+       (add-to-list 'lsp-language-id-configuration '(bash-ts-mode . "shellscript"))
 
        ;; Only display icons in GUI
        (defun my-lsp-icons-get-symbol-kind (fn &rest args)
@@ -327,16 +328,16 @@
             (dap-stopped    . (lambda (_) (dap-hydra)))
             (dap-terminated . (lambda (_) (dap-hydra/nil)))
 
-            (python-mode            . (lambda () (require 'dap-python)))
-            (ruby-mode              . (lambda () (require 'dap-ruby)))
-            (go-mode                . (lambda () (require 'dap-go)))
-            (java-mode              . (lambda () (require 'dap-java)))
-            ((c-mode c++-mode)      . (lambda () (require 'dap-lldb)))
-            ((objc-mode swift-mode) . (lambda () (require 'dap-lldb)))
-            (php-mode               . (lambda () (require 'dap-php)))
-            (elixir-mode            . (lambda () (require 'dap-elixir)))
-            ((js-mode js2-mode)     . (lambda () (require 'dap-chrome)))
-            (powershell-mode        . (lambda () (require 'dap-pwsh))))
+            ((python-mode python-ts-mode)            . (lambda () (require 'dap-python)))
+            ((ruby-mode ruby-ts-mode)                . (lambda () (require 'dap-ruby)))
+            ((go-mode go-ts-mode)                    . (lambda () (require 'dap-go)))
+            ((java-mode java-ts-mode jdee-mode)      . (lambda () (require 'dap-java)))
+            ((c-mode c-ts-mode c++-mode c++-ts-mode) . (lambda () (require 'dap-lldb)))
+            ((objc-mode swift-mode)                  . (lambda () (require 'dap-lldb)))
+            (php-mode                                . (lambda () (require 'dap-php)))
+            (elixir-mode                             . (lambda () (require 'dap-elixir)))
+            ((js-mode js2-mode js-ts-mode)           . (lambda () (require 'dap-chrome)))
+            (powershell-mode                         . (lambda () (require 'dap-pwsh))))
      :init (when (executable-find "python3")
              (setq dap-python-executable "python3")))
 
@@ -533,9 +534,9 @@
        (interactive)
        (when (and (executable-find "yapf") buffer-file-name)
          (call-process "yapf" nil nil nil "-i" buffer-file-name)))
-     :hook (python-mode . (lambda ()
-                            (require 'lsp-pyright)
-                            (add-hook 'after-save-hook #'lsp-pyright-format-buffer t t)))
+     :hook (((python-mode python-ts-mode) . (lambda ()
+                                              (require 'lsp-pyright)
+                                              (add-hook 'after-save-hook #'lsp-pyright-format-buffer t t))))
      :init (when (executable-find "python3")
              (setq lsp-pyright-python-executable-cmd "python3")))
 
@@ -567,12 +568,9 @@
 
    ;; Java
    (use-package lsp-java
-     :config
-     ;; current VSCode defaults
-     (setq lsp-java-vmargs '("-XX:+UseZGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx2G" "-Xms100m"))
-     :hook (java-mode . (lambda () (require 'lsp-java))))))
+     :hook ((java-mode java-ts-mode jdee-mode) . (lambda () (require 'lsp-java))))))
 
-(when (memq centaur-lsp '(lsp-mode eglot))
+(unless centaur-lsp
   ;; Enable LSP in org babel
   ;; https://github.com/emacs-lsp/lsp-mode/issues/377
   (cl-defmacro lsp-org-babel-enable (lang)
