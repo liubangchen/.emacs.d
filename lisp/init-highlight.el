9b6075f724ac319a086cb4eb595a21cf030db2d5
liubangchen <liubangchen@tencent.com>
2023-09-21 14:29:46 +0800

    禁用格式化

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -130,34 +130,34 @@ FACE defaults to inheriting from default and highlight."
     (advice-add #'deactivate-mark :after #'turn-on-symbol-overlay)))
 
 ;; Highlight indentions
-(use-package highlight-indent-guides
-  :diminish
-  :hook ((prog-mode yaml-mode) . (lambda ()
-                                   "Highlight indentations in small files for better performance."
-                                   (unless (too-long-file-p)
-                                     (highlight-indent-guides-mode 1))))
-  :init (setq highlight-indent-guides-method 'character
-              highlight-indent-guides-responsive 'top
-              highlight-indent-guides-suppress-auto-error t)
-  :config
-  (with-no-warnings
-    ;; Don't display first level of indentation
-    (defun my-indent-guides-for-all-but-first-column (level responsive display)
-      (unless (< level 1)
-        (highlight-indent-guides--highlighter-default level responsive display)))
-    (setq highlight-indent-guides-highlighter-function
-          #'my-indent-guides-for-all-but-first-column)
-
-    ;; Disable in `macrostep' expanding
-    (with-eval-after-load 'macrostep
-      (advice-add #'macrostep-expand
-                  :after (lambda (&rest _)
-                           (when highlight-indent-guides-mode
-                             (highlight-indent-guides-mode -1))))
-      (advice-add #'macrostep-collapse
-                  :after (lambda (&rest _)
-                           (when (derived-mode-p 'prog-mode 'yaml-mode)
-                             (highlight-indent-guides-mode 1)))))))
+;;(use-package highlight-indent-guides
+;;  :diminish
+;;  :hook ((prog-mode yaml-mode) . (lambda ()
+;;                                   "Highlight indentations in small files for better performance."
+;;                                   (unless (too-long-file-p)
+;;                                     (highlight-indent-guides-mode 1))))
+;;  :init (setq highlight-indent-guides-method 'character
+;;              highlight-indent-guides-responsive 'top
+;;              highlight-indent-guides-suppress-auto-error t)
+;;  :config
+;;  (with-no-warnings
+;;    ;; Don't display first level of indentation
+;;    (defun my-indent-guides-for-all-but-first-column (level responsive display)
+;;      (unless (< level 1)
+;;        (highlight-indent-guides--highlighter-default level responsive display)))
+;;    (setq highlight-indent-guides-highlighter-function
+;;          #'my-indent-guides-for-all-but-first-column)
+;;
+;;    ;; Disable in `macrostep' expanding
+;;    (with-eval-after-load 'macrostep
+;;      (advice-add #'macrostep-expand
+;;                  :after (lambda (&rest _)
+;;                           (when highlight-indent-guides-mode
+;;                             (highlight-indent-guides-mode -1))))
+;;      (advice-add #'macrostep-collapse
+;;                  :after (lambda (&rest _)
+;;                           (when (derived-mode-p 'prog-mode 'yaml-mode)
+;;                             (highlight-indent-guides-mode 1)))))))
 
 ;; Colorize color names in buffers
 (use-package rainbow-mode
