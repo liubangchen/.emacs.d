8d56a5adb701d820c853e6a1f6ffc046be809d5f
liubangchen <liubangchen@tencnet.com>
2024-06-24 17:19:47 +0800

    设置org dir

--- lisp/init-config.el
+++ lisp/init-config.el
@@ -1,54 +1,10 @@
+;;(defalias 'yes-or-no-p 'y-or-no-p)
+;;(setq confirm-kill-emacs 'y-or-no-p)
 
-
-;; 语法高亮
-(global-font-lock-mode t)
-;; 以 y/n代表 yes/no
-(fset 'yes-or-no-p 'y-or-n-p)
-;; 显示括号匹配
-(scroll-bar-mode 0)
-(global-hl-line-mode 1)
-(tool-bar-mode 0)
-(show-paren-mode t)
-(setq show-paren-style 'parentheses)
-;;;; 显示时间，格式如下
-(display-time-mode 1)
-(setq display-time-24hr-format t)
-(setq inhibit-startup-message t)
-(setq display-time-day-and-date t)
-(transient-mark-mode t)
-;; 支持emacs和外部程序的粘贴
-(setq x-select-enable-clipboard t)
-;; 不产生备份文件
-(setq make-backup-files nil)
 ;;不产生临时文件
 (setq-default make-backup-files nil)
-
-(setq-default cursor-type '(bar . 1))
-;;设置光标形状
-(setq default-cursor-type 'bar)
-(set-cursor-color "#ffffff")                    ;;设置备份文件路径
 (setq backup-directory-alist (quote (("." . "~/backups"))))
-
-
-(defun no-junk-please-were-unixish ()
-  (let ((coding-str (symbol-name buffer-file-coding-system)))
-    (when (string-match "-\\(?:dos\\|mac\\)$" coding-str)
-      (setq coding-str
-            (concat (substring coding-str 0 (match-beginning 0)) "-unix"))
-      (message "CODING: %s" coding-str)
-      (set-buffer-file-coding-system (intern coding-str)) )))
-(add-hook 'find-file-hooks 'no-junk-please-were-unixish)
-
-(define-key isearch-mode-map [escape] 'isearch-abort)   ;;;; isearch
 (global-set-key [escape] 'keyboard-escape-quit)         ;;;; everywhere else
-;;设置选中区域的颜色
-;;(set-face-attribute 'region nil :background "#99CC00" :foreground "#ffffff")
-;;(setq helm-locate-command "locate %s  -d /Users/chenlong/locate.database -l 100  %s")
-;;(setq yas-snippet-dirs '("~/.emacs.d/snippets/emacs-snippets"))
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-
 
 (use-package logview
   :init
