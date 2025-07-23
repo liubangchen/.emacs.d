fc75a92f4203c7e7a901ec10213e9e4d78164c18
liubangchen <20183398@qq.com>
2025-07-23 18:48:46 +0800

    taskjuggle-调试

--- init.el
+++ init.el
@@ -173,6 +173,6 @@ Avoid placing large files like EAF in `site-lisp` to prevent slow startup."
 (require 'init-ollama)
 (require 'init-jupyter)
 (require 'init-orgtools)
-(require 'init-ai)
+(require 'init-ml)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
